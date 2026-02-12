6812c29303a92faa9214fd46cecbf5c1365be574
vctrmmnlbrbn <victoremmanuelburbano@gmail.com>
2026-02-12 18:33:57 +0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,4 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-
-  // Base path for GitHub Pages deployment
-  base: '/valentine-proposal/',
 })
